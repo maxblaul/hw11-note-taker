9d3854feb2953c602edf9cf48fc4bf78da879878
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-21 18:30:48 -0500

    worked on server.js, app post get and delete

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
+const fs = require('fs');
 const express = require("express");
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require('./db/db.json');
+
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.static('public'))
 
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let dbData = JSON.parse(data);
+        res.json(dbData)
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+
+    newNote.id = uuidv4()
+
+    db.push(newNote)
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+
+    res.json(db)
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const newDb = db.filter((note) =>
+    note.id !== req.params.id)
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
+
+    readFile.json(newDb)
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
 app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`))
+
