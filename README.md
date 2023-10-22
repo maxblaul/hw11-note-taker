a3bfb74ee4cd4d667781f42c49d298d7238a5479
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-21 22:27:36 -0500

    added text to readme file

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # hw11-note-taker
 note taker assignment
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Contents
+  * [Description](#Description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Repo Link](#repolink)
+  * [Screenshot](#screenrecording)
+  * [Resources](#resources)
+
+
+## Description 
+
+This is a express.js application that allows a user to take notes and have them saved to or removed from a database.
+
+## Installation 
+
+Simply clone the repository in your terminal. Package Json and all other relevant modules are already installed and present in the folder.
+
+## Usage
+
+This app is designed to take notes. In order to run this application, user must open the terminal, navigate to the file, and write the command 'node server.js'. The terminal will populate a link to the local host. This link opens in the default browser and displays the application.
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Tests
+
+No test file in the app. If you wish to create a test, simply make a test file and describes different notes you want to test for.
+
+## Questions
+
+Any questions about the app, you may email the developer: blaulmax@gmail.com
+
+## Repo Link
+
+https://peaceful-dawn-76039-8bfab46614bf.herokuapp.com/notes
+
+https://github.com/maxblaul/hw11-note-taker-maxblaul
+
+Not sure if this one is required but I'm going to include it in here anyway:
+
+https://maxblaul.github.io/hw11-note-taker-maxblaul/
+
+## Screenshot 
+
+![Screenshot image](image.png)
+
+## Resources
+
+Class notes. 
+I had some help from TA Sam Adams to set up boiler plate of server.js. 
+Looked at some youtube videos for strucutre and syntax help. This one was the most helpful/utilized:
+https://youtu.be/SccSCuHhOw0?si=q27FXKZGuhm8fvv1. 
+
+Used ChatGpt to screen for syntax typos.
+
--- server.js
+++ server.js
@@ -1,69 +1,75 @@
+// Import needed modules
 const fs = require('fs');
 const express = require("express");
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Initialize db variable with data from 'db.json' 
 let db = require('./db/db.json');
 
+// Import the 'uuid' module, this creates unique identifiers
 const { v4: uuidv4 } = require('uuid');
 
+// Middleware for parsing JSON, url encoded data, and serving static files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
+// Reads 'db.json' file and parses the data
 fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     db = JSON.parse(data);
 });
 
+// Handle GET request retrieving notes
 app.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
+// POST request to add new notes
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
 
+    // Generating unique id for new notes
     newNote.id = uuidv4();
 
+    // Push new note to 'db' array
     db.push(newNote);
 
+    // Writes changes to 'db' array to 'db.json'
     fs.writeFileSync('./db/db.json', JSON.stringify(db));
 
+    // Sends a response with new note updates
     res.json(db);
 });
 
+// DELETE request
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
 
+    // Filters the selected note to be deleted from 'db' array
     db = db.filter((note) => note.id !== noteId);
 
+    // Writes updates that reflect deletion
     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
         if (err) throw err;
         res.json(db);
     });
 });
-//  => {
-//     const newDb = db.filter((note) =>
-//     note.id !== req.params.id);
 
-//     fs.writeFile('./db/db.json', JSON.stringify(db). (err) => {
-//         if (err) throw err;
-//     });
-
-//     res.json(dbb);
-// });
-
+// Defining routes to serve HTML files
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
+
+// Catch-all route to serve the index.html file or any unmatched routes
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-
+// Start the Express server and listen on Local Host port (3001)
 app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`))
-
