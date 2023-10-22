79f355a46a0843b1e067cbd01af51aed76a6512d
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-21 20:55:21 -0500

    fixed delete notes feature

--- server.js
+++ server.js
@@ -3,52 +3,64 @@ const express = require("express");
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const db = require('./db/db.json');
+let db = require('./db/db.json');
 
 const { v4: uuidv4 } = require('uuid');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    db = JSON.parse(data);
+});
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) throw err;
-        let dbData = JSON.parse(data);
-        res.json(dbData)
-    });
+    res.json(db);
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body
+    const newNote = req.body;
 
-    newNote.id = uuidv4()
+    newNote.id = uuidv4();
 
-    db.push(newNote)
+    db.push(newNote);
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
 
-    res.json(db)
+    res.json(db);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const newDb = db.filter((note) =>
-    note.id !== req.params.id)
+    const noteId = req.params.id;
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
+    db = db.filter((note) => note.id !== noteId);
 
-    readFile.json(newDb)
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) throw err;
+        res.json(db);
+    });
 });
+//  => {
+//     const newDb = db.filter((note) =>
+//     note.id !== req.params.id);
+
+//     fs.writeFile('./db/db.json', JSON.stringify(db). (err) => {
+//         if (err) throw err;
+//     });
+
+//     res.json(dbb);
+// });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
